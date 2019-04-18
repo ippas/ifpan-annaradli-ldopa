c633fdbdb187ae639aeb5f06ecb2f59836365e37
gosborcz <48823029+gosborcz@users.noreply.github.com>
2019-04-18 17:16:09 +0200

    Update list-and-count-reads.sh

--- list-and-count-reads.sh
+++ list-and-count-reads.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-printf " | file_name | number_of_reads | \n --------" >> files_and_reads.md
+printf " | file_name | number_of_reads | \n | -------- | ------- |" >> files_and_reads.md
 for FILE in `ls *.fq.gz`
 do
 CUTS=`wc -l $FILE | cut -d " " -f 1`
 READS=`echo "$CUTS/4" | bc`
-printf "\n | $FILE | $READS | \n --------" >> files_and_reads.md
+printf "\n | $FILE | $READS | " >> files_and_reads.md
 done
