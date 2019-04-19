a713e19bc1da5021af0210843256b8c92555d6ab
gosborcz <48823029+gosborcz@users.noreply.github.com>
2019-04-19 11:14:53 +0200

    Update and rename list-and-count-reads.sh to generate-sample-info-table.sh

---
+++ generate-sample-info-table.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+printf " \n | file-name | number-of-lines | number-of-reads | qc-info | \n | -------- | ------- | --------- | -----------| " >> sample-info.md
+for FILE in `ls *.fq.gz`
+do
+CUTS=`wc -l $FILE | cut -d " " -f 1`
+READS=`echo "$CUTS/4" | bc`
+FILENAME=`echo $FILE | cut -d "/" -f 1`
+printf "\n | $FILENAME | $CUTS | $READS |  | " >> sample-info.md
+done
+printf "\n" >> sample-info.md
--- list-and-count-reads.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-printf " | file_name | number_of_reads | \n | -------- | ------- |" >> files_and_reads.md
-for FILE in `ls *.fq.gz`
-do
-CUTS=`wc -l $FILE | cut -d " " -f 1`
-READS=`echo "$CUTS/4" | bc`
-printf "\n | $FILE | $READS | " >> files_and_reads.md
-done
