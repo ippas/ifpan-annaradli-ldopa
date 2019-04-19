d5857209315b24b85250025c80b0b14621d7acc2
gosborcz <48823029+gosborcz@users.noreply.github.com>
2019-04-19 11:17:32 +0200

    Update generate-sample-info-table.sh

--- generate-sample-info-table.sh
+++ generate-sample-info-table.sh
@@ -4,7 +4,7 @@ for FILE in `ls *.fq.gz`
 do
 CUTS=`wc -l $FILE | cut -d " " -f 1`
 READS=`echo "$CUTS/4" | bc`
-FILENAME=`echo $FILE | cut -d "/" -f 1`
+FILENAME=`echo $FILE | cut -d "." -f 1`
 printf "\n | $FILENAME | $CUTS | $READS |  | " >> sample-info.md
 done
 printf "\n" >> sample-info.md
