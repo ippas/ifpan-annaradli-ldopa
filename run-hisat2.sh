2e65458a11303bb2daf1c12c38ea88f694fa7355
gosborcz <malgorzata.m.borczyk@gmail.com>
2019-04-26 07:15:13 +0200

    script to run hisat2 an all samples

---
+++ run-hisat2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo touch temp.txt
+sudo chmod 777 temp.txt
+sudo touch temp2.txt
+sudo chmod 777 temp2.txt
+for FILE in `ls fq/*.fq.gz fq`
+do
+echo $FILE | cut -d "_" -f 1 >> temp.txt
+done
+sort temp.txt | uniq >> temp2.txt
+sudo rm temp.txt
+for FILE in `cat temp2.txt`
+do
+FILE1=$FILE"_1.fq.gz"
+FILE2=$FILE"_2.fq.gz"
+docker run --rm -v $PWD:/data zlskidmore/hisat2:2.1.0 hisat2 -x /data/rn6-ind/genome -1 /data/fq/$FILE1 -2 /data/fq/$FILE2 -S /data/$FILE.sam
+echo I have run hisat2 on $FILE
+done
+sudo rm temp2.txt
