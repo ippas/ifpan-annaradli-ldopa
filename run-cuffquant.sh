b426951334d587403fec6780061e2d36f2289876
gosborcz <malgorzata.m.borczyk@gmail.com>
2019-04-30 18:58:26 +0200

    minor detail

--- run-cuffquant.sh
+++ run-cuffquant.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for FILE in `ls sam/*.sort.sam`
+for FILE in `ls bam/*.sort.sam`
 do docker run -d --rm -v $PWD:/data octavianus90/cufflinks_final:latest cuffquant /data/rn6-ind/Rattus_norvegicus.Rnor_6.0.90.gtf /data/$FILE
 echo I have started  cuffquant on $FILE
 done
