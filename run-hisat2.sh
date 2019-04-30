b7b6a1a4aa101211e71671a7808bf876de66bd9a
gosborcz <malgorzata.m.borczyk@gmail.com>
2019-04-30 15:24:37 +0200

    update

--- run-hisat2.sh
+++ run-hisat2.sh
@@ -13,7 +13,7 @@ for FILE in `cat temp2.txt`
 do
 FILE1=$FILE"_1.fq.gz"
 FILE2=$FILE"_2.fq.gz"
-docker run -d --rm -v $PWD:/data zlskidmore/hisat2:2.1.0 hisat2 -x /data/rn6-ind/genome -1 /data/fq/$FILE1 -2 /data/fq/$FILE2 -S /data/$FILE.sam --summary-file /data/$FILE.txt
+docker run -d --rm -v $PWD:/data zlskidmore/hisat2:2.1.0 hisat2 -x /data/rn6-ind/genome -1 /data/fq/$FILE1 -2 /data/fq/$FILE2 -S /data/$FILE.sam --summary-file /data/$FILE.txt -dta-cufflinks
 echo I have run hisat2 on $FILE
 done
 sudo rm temp2.txt
