b08851a7e284bf7d467f15bd75f6564bdd8ca649
gosborcz <malgorzata.m.borczyk@gmail.com>
2019-04-30 17:06:32 +0200

    bam as output

--- sort-sam.sh
+++ sort-sam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for FILE in `ls *.sam | cut -d "." -f 1`
 do
-docker run -d --rm -v $PWD:/data zlskidmore/samtools:1.9 samtools sort -O sam -T /data/$FILE.sort -o /data/$FILE.sort.sam /data/$FILE.sam
+docker run -d --rm -v $PWD:/data zlskidmore/samtools:1.9 samtools sort -O bam -T /data/$FILE.sort -o /data/$FILE.sort.bam /data/$FILE.sam
 echo I have started sorting $FILE
 done
