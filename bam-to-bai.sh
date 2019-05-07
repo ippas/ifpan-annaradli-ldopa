1c51cdfb87207252c0fd85d9299fd9721dac7ee8
gosborcz <malgorzata.m.borczyk@gmail.com>
2019-05-07 19:05:51 +0200

    picard bam to bai

--- bam-to-bai.sh
+++ bam-to-bai.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for FILE in `ls *.bam`
 do
-docker run --rm -v $PWD:/usr/working broadinstitute/picard:latest BuildBamIndex I=$FILE O=$FILE.bai
+docker run -d --rm -v $PWD:/usr/working broadinstitute/picard:latest BuildBamIndex I=$FILE O=$FILE.bai
 done
